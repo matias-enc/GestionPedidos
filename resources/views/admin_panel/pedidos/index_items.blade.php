dc70c6a403ec5419226545978101adf92dd32cb9
Matias <matusalemn8@gmail.com>
2019-09-13 19:39:57 -0300

    Comprobacion de Disponibilidad terminada

---
+++ resources/views/admin_panel/pedidos/index_items.blade.php
@@ -0,0 +1,59 @@
+@extends('admin_panel.index')
+@section('content')
+    <div class="container">
+                    <form action="{{ route("pedidos.consultar_disponibilidad") }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+
+                                {{-- <nuevo_pedido>
+                                </nuevo_pedido> --}}
+                                <br>
+                                <div class="card card-outline card-primary col-5 offset-3">
+                                    <div class="card-header text-center">
+                                        <div class="card-title">
+                                            <h3><strong>Realizar un Nuevo Pedido</strong></h3>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <strong><Label>Seleccione una Categoria</Label></strong>
+                                        <div class="form-group">
+                                            <div class="form-group {{ $errors->has('tipoItem') ? 'has-error' : '' }}">
+                                                <select name="tipoItem" id="tipoItem" class="estados-js form-control" required>
+                                                    @foreach($tipoItems as $id => $tipo)
+                                                        <option  placeholder="Categoria"  value="{{ $id }}" >{{ $tipo }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-between">
+                                            <strong><Label>Llegada</Label></strong>
+                                            <strong><Label>Salida</Label></strong>
+                                        </div>
+                                            <div class="input-group input-daterange" id="datepicker" data-date-format="dd/mm/yyyy">
+                                                <input type="text" class="input-sm form-control" name="inicial" placeholder="Llegada" />
+                                                <input type="text" class="input-sm form-control" name="final" placeholder="Salida" />
+                                            </div>
+
+                                        <br>
+                                        <div class="d-flex justify-content-end">
+                                            <input class="btn btn-pill btn-success" type="submit" value="Consultar Disponibilidad">
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                    </form>
+
+        </div>
+
+
+    </div>
+@endsection
+@push('scripts')
+<script>
+    $('#datepicker').datepicker({
+        weekStart: 1,
+        daysOfWeekHighlighted: "6,0",
+        language: "es",
+        todayHighlight: true,
+    });
+</script>
+@endpush
